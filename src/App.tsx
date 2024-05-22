95e7753018cc7f66caa9bd4f5b3b7c6e0c59274c
Rizky Zakaria <rizky.zakaria78@gmail.com>
2024-05-22 17:50:28 +0700

    typing props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Greet } from './components/Greet';
 function App() {
   return (
     <div className="App">
-      <Greet name="Zaki" />
+      <Greet name="Rizky Zakaria" />
     </div>
   );
 }
